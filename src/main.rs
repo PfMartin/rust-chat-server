4d502cd9d70a9ff0a2e6f4ae29fa8407cd3ecb32
Martin <martinpfatrisch@gmail.com>
2023-04-18 14:17:56 +0200

    Added broadcast functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::TcpListener,
+    sync::broadcast,
 };
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
+    let (tx, _rx) = broadcast::channel(10);
+
     loop {
-        let (mut socket, _addr) = listener.accept().await.unwrap();
+        let (mut socket, addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (read, mut writer) = socket.split();
@@ -17,13 +23,25 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
-                }
+                tokio::select! {
+                    res = reader.read_line(&mut line) => {
+                        if res.unwrap() == 0 {
+                            break;
+                        }
 
-                writer.write_all(line.as_bytes()).await.unwrap();
-                line.clear();
+                        tx.send((line.clone(), addr)).unwrap();
+                        line.clear();
+                    }
+
+                    res = rx.recv() => {
+                        let (msg, other_addr) = res.unwrap();
+
+                        if addr != other_addr {
+                            writer.write_all(msg.as_bytes()).await.unwrap();
+                        }
+
+                    }
+                }
             }
         });
     }
